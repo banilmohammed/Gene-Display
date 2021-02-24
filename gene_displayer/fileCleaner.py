673e8d64cfdb7026e12909a10a820782706ff8b9
nabil <nabil@pop-os.localdomain>
2021-02-24 00:14:10 -0800

    added conda env yml as well as tried to parallelize api calls

--- gene_displayer/fileCleaner.py
+++ gene_displayer/fileCleaner.py
@@ -17,6 +17,7 @@ file as well as methods to clean the data file
 import re
 import pandas as pd
 import requests
+import concurrent.futures
 
 
 class SeriesData:
@@ -26,21 +27,52 @@ class SeriesData:
         with open(fileName, "r") as f:
             tableData = f.readlines()
             filterData = list(filter(reCompiled.match, tableData))
-            self.cleanedData = list(map(lambda x: x.split("\t"), filterData))
+        cleanedData = list(map(lambda x: x.split("\t"), filterData))
+        self.df = pd.DataFrame(cleanedData[3:], columns=cleanedData[1])
+        self.df.rename(columns={"!Sample_title": "affy_gene_probe_id"}, inplace=True)
+        self.geneAnnotateDf = pd.DataFrame(
+            columns=[
+                "affy_gene_probe_id",
+                "Gene Name",
+                "Gene Description",
+                "Homologene",
+                "GCC acc",
+                "RefSeq",
+            ]
+        )
+        self.affyProbeIDs = self.df["affy_gene_probe_id"].str.replace('"', "").to_list()
 
-    def toDataFrame(self):
-        df = pd.DataFrame(self.cleanedData[3:], columns=self.cleanedData[1])
-        return df
+    def myGeneCall(self, probe_id):
+        query = f"q=reporter:{probe_id}"
+        fields = "fields=symbol,name,refseq,homologene,accession"
+        url = f"http://mygene.info/v3/query?{query}&{fields}&size=1"
+        res = requests.get(url)
+        if len(res.json()["hits"]) != 0:
+            # for all the hardcoded stuff aka [0] ask about how to choose right one
+            jsonResponse = res.json()["hits"][0]
+            self.geneAnnotateDf = self.geneAnnotateDf.append(
+                {
+                    "affy_gene_probe_id": probe_id,
+                    "Gene Name": jsonResponse["symbol"],
+                    "Gene Description": jsonResponse["name"],
+                    "Homologene": jsonResponse["homologene"]["genes"][0][0],
+                    "GCC acc": jsonResponse["accession"]["genomic"][0],
+                    "RefSeq": jsonResponse["refseq"]["genomic"],
+                },
+                ignore_index=True,
+            )
+
+    def parallelAPI(self):
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            executor.map(self.myGeneCall, self.affyProbeIDs[:50])
+
+    def combineDataFrame(self, df1, df2):
+        merged_df = pd.merge(df1, df2, on="affy_gene_probe_id", how="outer")
+        return merged_df
 
 
 class GSE23006(SeriesData):
-    def geneAnnotator(self):
-        geneDataFrame = self.toDataFrame()
-        affyProbeIDs = geneDataFrame["!Sample_title"].str.replace('"', "").to_list()
-        headers = {"content-type": "application/x-www-form-urlencoded"}
-        params = f"q={','.join(affyProbeIDs[:900])}&scopes=reporter&fields=name,symbol"
-        res = requests.post("http://mygene.info/v3/query", data=params, headers=headers)
-        print(res.text)
+    pass
 
 
 class GSE8056(SeriesData):
@@ -52,5 +84,6 @@ class GSE460(SeriesData):
 
 
 if __name__ == "__main__":
-    test = GSE23006("GSE23006_series_matrix.txt")
-    test.geneAnnotator()
+    test = SeriesData("GSE23006_series_matrix.txt")
+    test.parallelAPI()
+    print(test.geneAnnotateDf.head())
