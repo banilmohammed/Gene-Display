e82c467e0aac626dfc06692610792a986377dd59
nabil <nabil@pop-os.localdomain>
2021-03-17 18:30:03 -0700

    documented code as well as updated env file

--- gene_displayer/commandLineParse.py
+++ gene_displayer/commandLineParse.py
@@ -1,45 +1,71 @@
 #!/usr/bin/env python3
 
+"""
+Module that makes use of argparse to create command line parsing class to deal with
+command line arguments.
+
+Classes:
+    CommandLineParse: class to parse out command line arguments provided by user
+"""
+
 import argparse
 
 
 class CommandLineParse:
+    """
+    Class to act as a wrapper for argparse and to initialize all the command line
+    arguments for this program.
+
+    Initialized: argument to toggle streamlit app to start, argument to indicate
+    input file needs to annotated, argument to indicate input file should be output
+    to csv, argument to indicate what input file is, argument to display version
+    information, argument to hold sample types within dataset variable to hold
+    value of all arguments
+    """
+
     def __init__(self):
+        # initialize argparse object
         self.parser = argparse.ArgumentParser(
             description="A simple program to annotate microarray data with gene names, symbols, refseq ids, and homologene ids as well as visualize gene concentrations.",
             add_help=True,
             prefix_chars="-",
         )
+        # toggle streamlit app
         self.parser.add_argument(
             "-s",
             "--see",
             action="store_true",
             help="Toggle gene comparison visualizer",
         )
+        # file needs to be annotated
         self.parser.add_argument(
             "-a",
             "--annotate",
             action="store_true",
             help="Annotate given file with gene symbol, gene description, homologene id, and refseq id",
         )
+        # file needs to be output as csv
         self.parser.add_argument(
             "-o",
             "--output",
             action="store_true",
             help="Output annotated file as csv",
         )
+        # name of input file
         self.parser.add_argument(
             "-i",
             "--input",
             action="store",
             help="Input file to be annotated or stored",
         )
+        # types within sample
         self.parser.add_argument(
             "-t",
             "--types",
             action="append",
             help="Input all the different types of samples included in data",
         )
+        # version information
         self.parser.add_argument(
             "-v", "--version", action="version", version="gene displayer 0.1.0"
         )
--- gene_displayer/fileCleaner.py
+++ gene_displayer/fileCleaner.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python3
 
 """
-Handles the file cleaning of GEO series data. Specifically GSE23006,
+Module that handles the file cleaning of GEO series data. Specifically GSE23006,
 GSE8056, GSE460.
 
 Classes:
-    SeriesData: parent class that will contain the parsed out data
-file as well as methods to clean the data file
-    GSE23006: child class of SeriesData, overrides the methods for
-    file cleaning according to how the GSE23006 data file requires
-    GSE8056: child class of SeriesData, overrides the methods for
-    file cleaning according to how the GSE8056 data file requires
-    GSE460: child class of SeriesData, overrides the methods for
-    file cleaning according to how the GSE460 data file requires
+    SeriesData: class that will contain the parsed out data file as well as methods to clean the data file
+    ExistingData: class that is used to serialized already annotated data into pickle objects
 """
 import re
 import pandas as pd
@@ -20,11 +14,28 @@ import mygene
 
 
 class SeriesData:
+    """
+    Class to contain all methods relating to parsing out raw data as well as calling
+    mygene api to annotate with extra information.
+
+    Initialized: boolean variable to hold whether data will be output as csv as well,
+    name of file, types of samples contained in the file, pandas dataframe to hold
+    filtered data, list of all affymetrix probe ids, pandas dataframe to hold new
+    information from mygene api, and finally pandas dataframe for the combination of
+    filtered data and new information from mygene api
+
+    Methods: myGeneCall(): query mygene api with affymetrix probe ids and store
+    resulting information in pandas dataframe, dataframeOutputter(): serialize
+    pandas dataframe into pickle object and export annotated data if output is
+    true, combineDataFrame(): outer merge both filtered data and new information
+    obtained from mygene api into a single pandas dataframe while dropping duplicates
+    """
+
     def __init__(self, output, fileName, sampleTypes):
-        self.output = output
+        self.output = output  # determines whether file is output as csv
         self.filename = fileName
         self.sampleTypes = sampleTypes
-        filenamePath = f"../infiles/{fileName}"
+        filenamePath = f"../infiles/{fileName}"  # relative path of file
         # regex for matching sample title and rows in data table
         regex = r"^!Sample(?=_title).*|^[^!I].*"
         reCompiled = re.compile(regex)  # compile regex for speed
@@ -42,13 +53,14 @@ class SeriesData:
         )
         # take out quotes from affy probe ids and convert it to list
         self.affyProbeIDs = self.df["affy_gene_probe_id"].str.replace('"', "").to_list()
+        # initialize empty object parameters here for clarity
         self.geneAnnotateDf = None
         self.combinedDf = None
 
     def myGeneCall(self):
+        """ Query MyGene API with all the affymetrix probe IDs and for each, get: gene symbol, gene name, refseq id, and homologene id. """
         mg = mygene.MyGeneInfo()
         # query with affy probe ids
-        # maybe add GenBank acc as well
         self.geneAnnotateDf = mg.querymany(
             self.affyProbeIDs,
             scopes="reporter",
@@ -97,20 +109,25 @@ class SeriesData:
         )
 
     def dataframeOutputter(self):
-        # MAKE SURE FOLDER NAME IS EXACT -- find way around this
+        """ Serialize combinded dataframe as pickle object located in data/ directory and output as csv if output is selected. """
+        # if there are more than one type of sample in a given dataset
         if len(self.sampleTypes) > 1:
             for sample in self.sampleTypes:
+                # filter out columns that have the sample that we are looking for
                 sampleColumns = self.combinedDf.columns[
                     ~self.combinedDf.columns.str.contains(sample)
                 ].to_list()
+                # output to csv file located in outfiles/
                 if self.output is True:
                     self.combinedDf[sampleColumns].to_csv(
                         f"../outfiles/{self.filename[:-4]}_{sample}.csv", index=False
                     )
+                # serialize dataframe to pickle object located in data/
                 self.combinedDf[sampleColumns].to_pickle(
                     f"../data/{self.filename[:-4]}_{sample}.pkl"
                 )
 
+        # same as above if there is only one sample type in given dataset
         else:
             if self.output is True:
                 self.combinedDf.to_csv(
@@ -119,25 +136,29 @@ class SeriesData:
             self.combinedDf.to_pickle(f"../data/{self.filename[:-4]}.pkl")
 
     def combineDataFrame(self):
+        """ Combine filtered data and new annotations from my gene api into one file. Also drop duplicated gene names. """
         # merge on probe ids and unionize keys of both dataframes
         self.combinedDf = pd.merge(
             self.geneAnnotateDf, self.df, on="affy_gene_probe_id", how="outer"
         )
 
+        # drop all nan
         self.combinedDf.dropna(axis=0, inplace=True)
+        # drop the column containing affymetrix probe ids, not needed anymore
         self.combinedDf.drop(["affy_gene_probe_id"], axis=1, inplace=True)
+        # drop duplicated gene names
         self.combinedDf.drop_duplicates(subset="Gene Name", inplace=True)
 
 
 class ExistingData:
+    """
+    Class to serialize pre cleaned data into pickle objects located in data/
+
+    Initialized: pandas dataframe that reads in csv of pre cleaned data
+    """
+
     def __init__(self, filename):
+        # read in csv
         self.existDf = pd.read_csv(f"../infiles/{filename}")
+        # serialize as pickle object located in data/
         self.existDf.to_pickle(f"../data/{filename[:-4]}.pkl")
-
-
-if __name__ == "__main__":
-    test = SeriesData("../infiles/GSE460_series_matrix.txt")
-    test.myGeneCall()
-    test.combineDataFrame()
-    # test.combinedDf.to_csv("output.csv", index=False)
-    test.dataframeSorter(["skin"])
